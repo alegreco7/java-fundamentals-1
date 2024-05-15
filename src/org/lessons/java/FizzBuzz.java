fa5f8d77f987c9d4b6c9dbf01f02e19372903b25
alegreco7 <alexgreco2002@gmail.com>
2024-05-15 15:14:02 +0200

    Esercizio 3: finito

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,2 +1,22 @@
-package org.lessons.java;public class FizzBuzz {
+package org.lessons.java;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        // Ciclo for che itera da 1 a 100
+        for (int i = 1; i <= 100; i++) {
+            // Verifica se i è un multiplo sia di 3 che di 5
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+                // Verifica se i è un multiplo solo di 3
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+                // Verifica se i è un multiplo solo di 5
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+                // Se i non è un multiplo né di 3 né di 5
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 }
