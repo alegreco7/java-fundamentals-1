ae303f91d41d8f0dcc4aa8a19fc8c76366004f6d
alegreco7 <alexgreco2002@gmail.com>
2024-05-15 17:28:36 +0200

    Esercizio 2: fix

--- src/org/lessons/java/FavoriteFoods.java
+++ src/org/lessons/java/FavoriteFoods.java
@@ -2,21 +2,25 @@ package org.lessons.java;
 
 public class FavoriteFoods {
     public static void main(String[] args) {
-        //Classifica cibi preferiti
+        // Classifica cibi preferiti
         String[] cibi = new String[5];
         cibi[0] = "pizza";
         cibi[1] = "pasta all'amatriciana";
         cibi[2] = "bistecca";
         cibi[3] = "carbonara";
         cibi[4] = "tortellini in brodo";
+
         // Stampare la lunghezza dell'array
         System.out.println("Lunghezza dell'array cibi: " + cibi.length);
+
         // Cibo top
         System.out.println("Il mio cibo preferito è: " + cibi[0]);
+
         // Cibo meno preferito
-        System.out.println("Il mio cibo meno preferito è: " + cibi[4]);
+        System.out.println("Il mio cibo meno preferito è: " + cibi[cibi.length - 1]);
+
         // Cibo di metà classifica
-        System.out.println("Il mio cibo di metà  classifica è: " + cibi[2]);
+        System.out.println("Il mio cibo di metà classifica è: " + cibi[cibi.length / 2]);
+
     }
 }
-
