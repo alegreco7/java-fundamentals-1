79904791e7a1094252b2edd7a1e654cc8eecf9cd
alegreco7 <alexgreco2002@gmail.com>
2024-05-16 13:28:16 +0200

    Esercizi Snack 16/5

---
+++ src/org/lessons/java/Snack6.java
@@ -0,0 +1,12 @@
+/*
+Snack BONUS
+Simulare il push di un array javascript usando però array java
+*/
+
+package org.lessons.java;
+
+public class Snack6 {
+    public static void main(String[] args) {
+
+    }
+}
